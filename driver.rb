85b7df53aa9a04c1cbc0bb600166c948d69196d6
Ddfulton <fulton.derek@gmail.com>
2016-07-17 21:34:03 -0400

    working drive for CHEM261 fall 2016

--- driver.rb
+++ driver.rb
@@ -1,63 +1,54 @@
 require 'capybara'
 require 'capybara/poltergeist'
 
-start = Time.now
+def enroll_chem261(onyen, password)
+    start = Time.now
 
-session = Capybara::Session.new(:poltergeist)
+    session = Capybara::Session.new(:poltergeist)
 
-session.visit "https://connectcarolina.unc.edu/"
-session.find('.loginbutton').click()
+    session.visit "https://connectcarolina.unc.edu/"
+    session.find('.loginbutton').click()
 
-puts("LOGGING IN...\n")
-session.find('#onyen').send_keys('ddfulton')
-session.find('#onyenPassword').send_keys('bojangles5\'')
-session.find('#action').click() # breaks on heroku
+    puts("LOGGING IN...\n")
+    session.find('#onyen').send_keys('ddfulton')
+    session.find('#onyenPassword').send_keys('bojangles5\'')
+    session.find('#action').click() # breaks on heroku
 
-puts("ENTERING SHOPPING CART...\n")
-#ENTER SHOPPING CART
-session.within_frame(session.find('#ptifrmtgtframe')) do
-    y = session.find("#ACE_DERIVED_SSS_SCL_SSS_ENRL_CART").click()
-end
-
-session.within_frame(session.find('#ptifrmtgtframe')) do 
-    class_table = session.find('table.PSLEVEL2GRIDWBO')
-    fall_2016 = class_table.find('span', text: '2016 Fall')[:id]
-    fall_2016_idx = fall_2016[-1]
-    puts(fall_2016_idx)
-    
-    fall_2016_xpath = "//*[@id=\"trSSR_DUMMY_RECV1$0_row2\"]/td[1]"
-    # click fall 2016
-    session.find(:xpath, fall_2016_xpath).click(); #TODO Improve and adapt for no term select page
-
-    # click continue
-    session.find("a#DERIVED_SSS_SCT_SSR_PB_GO").click()
-end
-
-#LIST OF CLASSES
-session.within_frame(session.find(:name, 'TargetContent')) do 
-    
-    class_table = session.find("table.SSSGROUPBOXDKBLUE") # works
-    puts(class_table.methods.sort)
-
-    #pertinent row ID is trSSR_REGFORM_VW$0_row1 (where 1 is any number)
-    i = 1
-    while class_table.find("tr#trSSR_REGFORM_VW$0_row" + i.to_s)
-        puts("Found row number %i", i)
+    session.within_frame(session.find('#ptifrmtgtframe')) do
+        session.find("#ACE_DERIVED_SSS_SCL_SSS_ENRL_CART").click()
     end
+
+    # Fall 2016 and Summer II 2016 Options — click fall 2016 if the table shows up
+    session.within_frame(session.find('#ptifrmtgtframe')) do 
+        class_table = session.find('table.PSLEVEL2GRIDWBO')
+        fall_2016 = class_table.find('span', text: '2016 Fall')[:id]
+        fall_2016_idx = fall_2016[-1]
+        puts(fall_2016_idx)
+        
+        fall_2016_xpath = "//*[@id=\"trSSR_DUMMY_RECV1$0_row2\"]/td[1]"
+        # click fall 2016
+        session.find(:xpath, fall_2016_xpath).click(); #TODO Improve and adapt for no term select page
+
+        # click continue
+        session.find("a#DERIVED_SSS_SCT_SSR_PB_GO").click()
+    end
+
+    # works pretty well
+    session.within_frame(session.find(:id, "ptifrmtgtframe")) do 
+        cart = session.find("table.PSLEVEL1GRIDNBO")
+        course = cart.find("input#P_SELECT\\$0")
+        course.click()
+
+        session.find("a#DERIVED_REGFRM1_LINK_ADD_ENRL").click()
+        session.find("a#DERIVED_REGFRM1_SSR_PB_SUBMIT").click()
+    end
+
+    sleep(1)
+    session.save_screenshot('final.png')
+
+    finish = Time.now
+    puts("\n\nTook you this long:")
+    puts(finish-start)
 end
 
-
-
-
-
-
-
-
-
-
-
-
-
-finish = Time.now
-puts("\n\nTook you this long:")
-puts(finish-start)
+enroll_chem261()
