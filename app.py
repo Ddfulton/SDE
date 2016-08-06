b451f44b559fe941a19322628e64f0431af1480f
Ddfulton <fulton.derek@gmail.com>
2016-08-06 15:38:49 -0400

    added simplejson and sendgrid processor

--- app.py
+++ app.py
@@ -9,29 +9,31 @@ app = Flask(__name__)
 
 
 
-@app.route('/', methods=["POST", "GET"])
+@app.route('/', methods=["GET"])
 
 def index():
-    if request.method == "POST":
-    	print("GOT THE MOTHATFUCKING SENDGRID. HERE IT IS %s" % json.loads(request.data))
-    	
-    	# try:
-    	# 	goods = request.data
-    	# except:
-    	# 	print("goods = request.data did not work :(")
-    	
-
-    	# try:
-    	# 	goods = request.data.decode('utf-8')
-    	# except:
-    	# 	print("goods = request.data.decode('utf-8')")
-
-    	
-    	email_driver.send_email('fulton.derek@gmail.com', 'You got a post', '%s' % request.data.decode('utf-8'))
-    else:
-        pass
     return render_template("index.html")
 
+@app.route('/parse', methods=["POST"])
+def parser():
+	# Required response to SendGrid.com’s Parse API
+	print("GOT THE SENDGRID")
+
+	# Consume the entire email
+	envelope = simplejson.loads(request.form.get('envelope'))
+	print(envelope)
+
+	# Get some header information
+	to_address = envelope['to'][0]
+	from_address = envelope['from']
+	print("From: %s" % (from_address))
+
+	# Now, onto the body
+	text = request.form.get('text')
+	html = request.form.get('html')
+	subject = request.form.get('subject')
+	print("Subject is %s" % (subject))
+	print("Text is %s" % (text))
 
 if __name__ == '__main__':
     app.run(debug=True)
