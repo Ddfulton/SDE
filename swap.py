44e90f341775326c0f320169308d505667d0bc2c
Ddfulton <fulton.derek@gmail.com>
2016-08-15 19:26:06 -0400

    email syntax

--- swap.py
+++ swap.py
@@ -41,7 +41,22 @@ def ajax():
 		driver.class_checker(goods['course'])
 
 
-		msg = "Dear %s,\nYou just signed up for %s.\nWelcome to Swap Drop Enroll. This service waits for an e-mail from classchecker, reads that email, \nand if the status changes from Closed to Open, it fetches your password and enrolls you. \nWe use three layers of security to protect your password. If you are interested, \nwe use a SOAP client with both a token and cipher, among other security features. \nFeel free to contact us at swapdropenroll@gmail.com.\nWarm regards, \nSwap Drop Enroll" % (onyen, goods['course'])
+		msg = """Dear %s,\n
+
+		You just signed up for %s.
+
+		Welcome to Swap Drop Enroll. This service waits for an e-mail from classchecker, reads that email, 
+		and if the status changes from Closed to Open, it fetches your password and enrolls you. 
+
+		We use three layers of security to protect your password. If you are interested, 
+		we use a SOAP client with both a token and cipher, among other security features. 
+
+		Feel free to contact us at swapdropenroll@gmail.com.
+
+		Warm regards, 
+
+		Swap Drop Enroll
+		""" % (goods['onyen'], goods['course'])
 
 		driver.send_email(goods['email'], 'Swap Drop Enroll', msg)
 	else:
