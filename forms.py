4fe0767a15d7f98bce52e0f159a23bff8bad533f
Ddfulton <fulton.derek@gmail.com>
2016-08-19 08:35:15 -0400

    fixed homepage

--- forms.py
+++ forms.py
@@ -1,7 +1,58 @@
-import subprocess
+<div class="col-sm-4" id="main">
+      <div id="signup">
+        <div class="box" data-step="1" id="content1">
+          <form class="center-box">
+            <h3 class="text-center no-top-margin">Cop the perfect schedule</h3>
 
-p = subprocess.Popen(["ruby", "verifyOnyen.rb", "ddfulton", "bojangles5'"], stdout=subprocess.PIPE)
+              <div class="form-group">
 
-out, err = p.communicate()
+                  <label for="signup_step_1_email" class="control-label">Onyen</label>
 
-print(out[-3:].decode('utf-8'))
+                  <div class="row-xs-10">
+                      <input type="email" class="form-control" id="onyenInput" name="email" placeholder="Michael Jeffrey Jordan">
+                  </div>
+              </div>
+
+              <div class="form-group">
+
+                  <label for="signup_step_1_password" class="control-label">Password</label>
+
+                  <div class="row-xs-10">
+                      <input type="password" class="form-control" id="onyenPassword" name="password" placeholder="paperClasses1">
+                  </div>
+              </div>
+
+              <div class="form-group">
+
+                  <label for="signup_step_1_email" class="control-label">Email</label>
+
+                  <div class="row-xs-10">
+                      <input type="email" class="form-control" id="emailInput" name="email" placeholder="kanye.west@live.unc.edu">
+                  </div>
+              </div>
+
+              <div class="form-group">
+
+                  <label for="signup_step_1_email" class="control-label">Course</label>
+
+                  <div class="row-xs-10">
+                      <input type="email" class="form-control" id="desiredCourse" name="desiredCourse" placeholder="AAAD 101-001">
+                  </div>
+              </div>
+
+              <div class="form-group last-form-group text-center">
+                <button id="click" type="button" class="btn btn-primary" data-next="true">Achieve my academic dreams</button>
+              </div>
+              </div>
+          </form>
+        </div>
+            
+    <div class="box" data-step="2" style="display:none" id="content2">
+    <form class="form-horizontal center-box">
+      <h3 class="text-center no-top-margin">
+        <img id="MJ" src="static/MJ.jpg">Check your e-mail!</img>
+      </h3>
+    </form>
+  </div>
+</div>
+    </div>
