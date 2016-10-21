d6c34846c6ae2b47bb58527a75444fc32c39a432
Derek Fulton <fulton.derek@gmail.com>
2016-10-21 17:07:22 +0200

    Added comprehensive descriptions for each method

--- newClient.py
+++ newClient.py
@@ -11,27 +11,41 @@ CX = pymysql.connect(host='gs-db-cluster1.cluster-clcutdgbykfx.us-east-1.rds.ama
 """
 These are the methods we need to write. 
 They all return different things to swap.py
-
 """
-def getLoginInfo(_desiredOnyen):
-    return None
-
-def registerOnyen(_onyen, _password, _email):
-    return None
-
-def registerClass(_onyen, _course):
+def registerCourse(_onyen, _password, _email, _course _referringOnyen=None):
+    """
+    Check if the user has already registered for this course. If so, 
+    reject them. Get their score from this query to use later.
+    If not, add them with the proper score.
+    Finally, give more points to the referring onyen.
+    """
     return None
 
 def getNextUser(_course):
+    """
+    This method needs to be faster than greased lightning.
+    Query all rows with the proper _course and success == 0
+    then use the weighting process to decide who gets the attempt. 
+    Returns the onyen and password.
+    """
     return None
 
 def getRegisteredClasses(_onyen):
+    """
+    Query for all the classes for which an onyen is currently registered.
+    """
     return None
 
 def markEnrollPass(_onyen, _course):
+    """
+    Change success from 0 to 1. E-mail them telling them they got in.
+    """
     return None
 
 def deleteUser(_onyen, _password):
+    """
+    Delete the bitch.
+    """
     return None
 
 def getOnyenInfo(_onyen):
